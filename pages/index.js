22ce3720e56db8eef282dbd625d666716dd45d4d
Mat Jordan <mat@northwestern.edu>
2022-10-28 18:53:02 -0400

    Kickstart.

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
   const id = "https://manifests.collections.yale.edu/ycba/obj/21168";
   return (
     <div>
+      Rendering... {id}
       <CloverIIIF id={id} />
     </div>
   );
