2be8bac32dd2a27618816bd708ef1c6c33e1e743
Mat Jordan <mat@northwestern.edu>
2022-10-29 07:10:27 -0400

    Test deploy v1.12.4-rc.0.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import dynamic from "next/dynamic";
 
-const CloverDynamic = dynamic(() => import("clover-mat"), {
+const CloverDynamic = dynamic(() => import("@samvera/clover-iiif"), {
   ssr: false,
 });
 export default function Home() {
   const id = "https://manifests.collections.yale.edu/ycba/obj/21168";
   return (
     <div>
-      Rendering... {id}...
+      Rendering... {id}... as v1.12.4-rc.0
       <CloverDynamic id={id} />
     </div>
   );
