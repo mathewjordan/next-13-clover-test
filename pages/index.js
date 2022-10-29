e2c4853e383c32c3a8564964ab2194815da73f78
Mat Jordan <mat@northwestern.edu>
2022-10-28 20:05:39 -0400

    Test w/o dynamic import.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import dynamic from "next/dynamic";
+import Clover from "@samvera/clover-iiif";
 
-const CloverIIIF = dynamic(() => import("@samvera/clover-iiif"), {
-  ssr: false,
-});
+// const Clover = dynamic(() => import("@samvera/clover-iiif"), {
+//   ssr: false,
+// });
 export default function Home() {
   const id = "https://manifests.collections.yale.edu/ycba/obj/21168";
   return (
     <div>
-      Rendering... {id}
-      <CloverIIIF id={id} />
+      Rendering... {id}...
+      <Clover id={id} />
     </div>
   );
 }
